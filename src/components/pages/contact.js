b7e1a4b9b36c6c7f35213b7b3e1227b6314bd1ec
Ali reza <aliraza.r10@gmail.com>
2023-02-27 00:44:14 +0500

    formSpree Added to contact form

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,16 @@
+'use client';
+
 import ResumeIcon from "@/assets/icons/resume-icon";
 import { SOCIAL_LINKS } from "@/content/constants";
 import { AiFillInstagram } from "react-icons/ai";
 import { IoLogoFacebook, IoLogoGithub, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contact () {
+    const [state, handleSubmit] = useForm("xoqzwabz");
+    if (state.succeeded) {
+        return <p>Thanks for the message and your time!</p>;
+    }
     return (
         <div className="min-h-screen flex flex-col justify-center max-w-3xl mx-auto">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
@@ -14,7 +21,7 @@ export default function Contact () {
                     </p>
                 </div>
                 <div>
-                    <form action="">
+                    <form method="POST" onSubmit={handleSubmit}>
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
                             <div className="">
                                 <label htmlFor="first-name" className="block text-sm font-medium text-primary-50">
@@ -55,10 +62,16 @@ export default function Contact () {
                                         defaultValue={''}
                                     />
                                 </div>
+                                <ValidationError 
+                                    prefix="Message" 
+                                    field="message"
+                                    errors={state.errors}
+                                />
                             </div>
                         </div>
                         <div className="mt-3 flex justify-end">
                         <button
+                            disabled={state.submitting}
                             type="button"
                             className="inline-flex justify-center rounded-md border border-transparent bg-accent-500 py-2 px-4 text-sm font-medium text-primary-50 shadow-sm hover:bg-accent-700 focus:outline-none focus:ring-1 focus:ring-accent-500 focus:ring-offset-1"
                         >
