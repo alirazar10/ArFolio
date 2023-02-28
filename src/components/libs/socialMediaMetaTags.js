ee08e9e13ae204e4175686d728d07a9fe50e0088
Ali reza <aliraza.r10@gmail.com>
2023-03-01 03:31:14 +0500

    meta tag improved

--- src/components/libs/socialMediaMetaTags.js
+++ src/components/libs/socialMediaMetaTags.js
@@ -1,29 +1,32 @@
 import { META_TAGS } from '@/content/metaTag'
 
 export default function SocialMediaMetaTags () {
-    const imageUrl = process.env.NEXT_PUBLIC_BASE_URL
-    return (
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    const title = `I am ${META_TAGS.title}`.toString()
+    console.log( typeof title);
+    return ( 
         <>
-            <title>I am {META_TAGS.title}</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
-            <meta name="description" content="Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more." />
+            <title>{title}</title>
+            <meta content="width=device-width, initial-scale=1 shrink-to-fit=no" name="viewport" />
+            <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+            <meta name="description" content={META_TAGS.description} />
+            <meta name="google-site-verification" content={`${baseUrl}/google1ecc11e592c8c73a.html`}/>
+            <meta name="keywords" content="Full Stack Developer, Next.js React.js, JavaScript, PHP, Laravel, Python, Flask, TailwindCSS, CSS, HTML, Developer, " />
+            <meta name="author" content="Ali Reza Rezayee"/>
             <link rel="icon" href="" />
-              {/* Facebook and linkedin */}
+              {/* Facebook and linkedin Meta Tags*/}
             <meta property={`og:title`} content={META_TAGS.facebook.title}/>
             <meta property={`og:description`} content={META_TAGS.facebook.description}/>
-            <meta property={`og:image`} content={META_TAGS.facebook.url+META_TAGS.facebook.image}/>
+            <meta property={`og:image`} content={baseUrl+META_TAGS.facebook.image}/>
             <meta property={`og:url`} content={ META_TAGS.facebook.url}/>
             <meta property={`og:type`} content={`website`}/>
 
-            {/* Twitter */}
-
+            {/* Twitter Meta Tags */}
             <meta name="twitter:card" content={META_TAGS.twitter.card}/>
             <meta name="twitter:site" content={META_TAGS.twitter.site} />
             <meta name="twitter:title" content={META_TAGS.twitter.creator}/>
-            <meta name="twitter:description" content={META_TAGS.twitter.card}/>
-            <meta name="twitter:image" content={imageUrl+META_TAGS.twitter.image}/>
-
-
+            <meta name="twitter:description" content={META_TAGS.twitter.description}/>
+            <meta name="twitter:image" content={baseUrl+META_TAGS.twitter.image.src}/>
         </>
     )
 }
--- src/content/metaTag.js
+++ src/content/metaTag.js
@@ -1,12 +1,13 @@
 export const META_TAGS = {
     "title": "Ali - Full Stack Developer",
-    "description": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more.",
+    "description": "Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more.",
     "image": "assets/images/4E6A5969.jpeg",
     "url": "https://iamali.vercel.app",
     "twitter": {
-      "card": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
+      "card": "summary_large_image",
       "site": "@AlirazaR10",
       "creator": "@AlirazaR10",
+      "description":"Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
       "image": {
         "src": "assets/images/4E6A5969.jpeg",
         "alt": "Ali Reza - Full Stack Developer"
@@ -14,25 +15,25 @@ export const META_TAGS = {
     },
     "facebook": {
       "title": "Ali Reza - Full Stack Developer",
-      "description": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
+      "description": "Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
       "url": "https://iamali.vercel.app/",
       "image": "assets/images/4E6A5969.jpeg"
     },
     "linkedin": {
       "title": "Ali Reza - Full Stack Developer",
-      "description": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
+      "description": "Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
       "url": "https://iamali.vercel.app/",
       "image": "assets/images/4E6A5969.jpeg"
     },
     "instagram": {
       "title": "Ali Reza - Full Stack Developer",
-      "description": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
+      "description": "Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
       "url": "https://iamali.vercel.app/",
       "image": "assets/images/4E6A5969.jpeg"
     },
     "tiktok": {
       "title": "Ali Reza - Full Stack Developer",
-      "description": "Experienced Full Stack Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
+      "description": "Ali Reza is Experienced Full Stack Web Developer proficient in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Python, Flask, Flutter, and more.",
       "url": "https://iamali.vercel.app/",
       "image": "assets/images/4E6A5969.jpeg"
     }
