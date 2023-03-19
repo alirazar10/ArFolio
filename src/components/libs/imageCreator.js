90de1559e12eac4ffbb73cc7ccb5e876a5b7c660
Ali reza <aliraza.r10@gmail.com>
2023-03-20 03:07:16 +0500

    image improved

--- src/components/libs/imageCreator.js
+++ src/components/libs/imageCreator.js
@@ -1,11 +1,11 @@
-import { fill } from "@cloudinary/url-gen/actions/resize";
+import { fill, limitFill } from "@cloudinary/url-gen/actions/resize";
 import { cld } from "../../configs/clConfig";
-import { Format, Quality } from "@cloudinary/url-gen/qualifiers";
 import { format, quality } from "@cloudinary/url-gen/actions/delivery";
+import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 
 export const generateImageUrl = (imagePublicId, width = 0) =>
   cld
     .image(imagePublicId)
-    .resize(fill())
+    .resize(fill().width("0.5").gravity(autoGravity()))
     .delivery(format("auto"))
     .delivery(quality("auto"));
