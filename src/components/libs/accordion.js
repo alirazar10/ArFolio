b548c44365422e33f7226784f3c7a3f2c012afae
Ali reza <aliraza.r10@gmail.com>
2023-04-02 18:34:15 +0500

    experience transition ref

--- src/components/libs/accordion.js
+++ src/components/libs/accordion.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import { MdAdd, MdOutlineOpenInNew, MdRemove } from "react-icons/md";
 import { IoLocation } from "react-icons/io5";
@@ -25,87 +25,93 @@ export const Accordion = ({ items }) => {
   return (
     <div className=" rounded-md w-full">
       <TransitionGroup component={null}>
-        {items?.map((item, index) => (
-          <CSSTransition
-            key={index}
-            in={true}
-            timeout={2000}
-            classNames="fadeup"
-            appear
-          >
-            <div
+        {items?.map((item, index) => {
+          const itemsRes = createRef(null);
+          return (
+            <CSSTransition
               key={index}
-              className="mb-4"
-              style={{ transitionDelay: `${index + 1}00ms` }}
+              in={true}
+              nodeRef={itemsRes}
+              timeout={2000}
+              classNames="fadeup"
+              appear
             >
               <div
-                className={`w-full text-left px-4 py-2 font-medium focus:outline-none group bg-primary-800 ${
-                  activeIndex === index ? "bg-opacity-40" : "bg-opacity-100"
-                } rounded-md hover:shadow-primary cursor-pointer`}
-                onClick={() => toggleItem(index)}
-              >
-                <div className="flex items-center justify-between py-2 gap-2">
-                  <p className="flex flex-col items-start justify-between gap-1.5 w-full text-sm lg:text-[1.05rem] font-semibold text-primary-100">
-                    <span className={``}>{item.title}</span>
-                    <span className={`text-xs lg:text-sm font-medium `}>
-                      {item.start_date} - {item.end_date}
-                    </span>
-                  </p>
-                  <span className="py-2">
-                    {index !== activeIndex ? (
-                      <MdAdd className="text-2xl text-primary-50 font-bold  transition ease-in-out duration-300" />
-                    ) : (
-                      <MdRemove className="text-2xl text-primary-50 font-bold" />
-                    )}
-                  </span>
-                </div>
-              </div>
-              <div
-                className={`overflow-hidden ease-in-out transition-[height]  duration-500`}
                 id={index}
-                style={
-                  activeIndex === index
-                    ? { height: activeHeight + 10 }
-                    : { height: 0 }
-                }
+                key={index}
+                ref={itemsRes}
+                className="mb-4"
+                style={{ transitionDelay: `${index + 1}00ms` }}
               >
-                <div className="px-4 py-4 mt-2 bg-secondary-500 text-primary-100 text-sm lg:text-base  text-justify rounded-md h-fit">
-                  <p className="text-sm text-secondary-300 pb-3 flex items-center gap-2">
-                    <IoLocation size={20} className="text-primary-50" />{" "}
-                    <span>{item.location}</span>
-                  </p>
-                  <p className="text-sm text-secondary-300 pb-4 flex items-center gap-2">
-                    {item.link && (
-                      <>
-                        <MdOutlineOpenInNew
-                          size={20}
-                          className="text-primary-50"
-                        />{" "}
-                        <a href={item.link} aria-label={"Website link"}>
-                          {item.link}
-                        </a>
-                      </>
-                    )}
-                  </p>
-                  <p className="text-sm text-primary-100">
-                    {" "}
-                    {item.description}{" "}
-                  </p>
-                  <div className="flex gap-2 flex-wrap mt-2.5">
-                    {item.skills.map((skill, index) => (
-                      <span
-                        key={index}
-                        className="px-2.5 py-2 bg-primary-100 text-secondary-300 text-xs rounded-xl bg-opacity-20"
-                      >
-                        {skill}
+                <div
+                  className={`w-full text-left px-4 py-2 font-medium focus:outline-none group bg-primary-800 ${
+                    activeIndex === index ? "bg-opacity-40" : "bg-opacity-100"
+                  } rounded-md hover:shadow-primary cursor-pointer`}
+                  onClick={() => toggleItem(index)}
+                >
+                  <div className="flex items-center justify-between py-2 gap-2">
+                    <p className="flex flex-col items-start justify-between gap-1.5 w-full text-sm lg:text-[1.05rem] font-semibold text-primary-100">
+                      <span className={``}>{item.title}</span>
+                      <span className={`text-xs lg:text-sm font-medium `}>
+                        {item.start_date} - {item.end_date}
                       </span>
-                    ))}
+                    </p>
+                    <span className="py-2">
+                      {index !== activeIndex ? (
+                        <MdAdd className="text-2xl text-primary-50 font-bold  transition ease-in-out duration-300" />
+                      ) : (
+                        <MdRemove className="text-2xl text-primary-50 font-bold" />
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <div
+                  className={`overflow-hidden ease-in-out transition-[height]  duration-500`}
+                  id={index}
+                  style={
+                    activeIndex === index
+                      ? { height: activeHeight + 10 }
+                      : { height: 0 }
+                  }
+                >
+                  <div className="px-4 py-4 mt-2 bg-secondary-500 text-primary-100 text-sm lg:text-base  text-justify rounded-md h-fit">
+                    <p className="text-sm text-secondary-300 pb-3 flex items-center gap-2">
+                      <IoLocation size={20} className="text-primary-50" />{" "}
+                      <span>{item.location}</span>
+                    </p>
+                    <p className="text-sm text-secondary-300 pb-4 flex items-center gap-2">
+                      {item.link && (
+                        <>
+                          <MdOutlineOpenInNew
+                            size={20}
+                            className="text-primary-50"
+                          />{" "}
+                          <a href={item.link} aria-label={"Website link"}>
+                            {item.link}
+                          </a>
+                        </>
+                      )}
+                    </p>
+                    <p className="text-sm text-primary-100">
+                      {" "}
+                      {item.description}{" "}
+                    </p>
+                    <div className="flex gap-2 flex-wrap mt-2.5">
+                      {item.skills.map((skill, index) => (
+                        <span
+                          key={index}
+                          className="px-2.5 py-2 bg-primary-100 text-secondary-300 text-xs rounded-xl bg-opacity-20"
+                        >
+                          {skill}
+                        </span>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </CSSTransition>
-        ))}
+            </CSSTransition>
+          );
+        })}
       </TransitionGroup>
     </div>
   );
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -43,10 +43,10 @@ export default function About() {
             </p>
           </div>
         </ScrollRevealWrapper>
-        <div className=" group rounded-md  lg:absolute lg:top-2/4 lg:-translate-y-2/4 lg:left-[50%] ">
+        <div className=" rounded-md  lg:absolute lg:top-2/4 lg:-translate-y-2/4 lg:left-[50%] ">
           <ScrollRevealWrapper options={{ origin: "right", delay: 200 }}>
             <div className="lg:bg-secondary-600 px-8 py-10 lg:shadow-primary lg:border lg:border-primary-700 rounded-md">
-              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-primary-50 pb-5 transition-all duration-300 group-hover:text-accent-500">
+              <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-primary-50 pb-5 transition-all duration-300 ">
                 Skills and Expertise
               </h2>
               <Skills skills={skills} />
