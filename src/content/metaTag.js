b8ab73f43f7f019e9c2642873aeff9b5688e446b
Ali reza <aliraza.r10@gmail.com>
2023-05-03 04:25:12 +0500

    image meta tag url update

--- src/content/metaTag.js
+++ src/content/metaTag.js
@@ -1,41 +1,46 @@
 export const META_TAGS = {
-    "title": "Ali - Full Stack Developer",
-    "description": "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-    "image": "assets/images/4E6A5969.jpeg",
-    "url": "https://iamali.vercel.app",
-    "twitter": {
-      "card": "summary_large_image",
-      "site": "@AlirazaR10",
-      "creator": "@AlirazaR10",
-      "description":"Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-      "image": {
-        "src": "assets/images/4E6A5969.jpeg",
-        "alt": "Ali Reza - Full Stack Developer"
-      }
+  title: "Ali - Full Stack Developer",
+  description:
+    "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+  image: "assets/images/4E6A5969.jpeg",
+  url: "https://iamali.vercel.app",
+  twitter: {
+    card: "summary_large_image",
+    site: "@AlirazaR10",
+    creator: "@AlirazaR10",
+    description:
+      "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+    image: {
+      src: "assets/images/4E6A5969.jpeg",
+      alt: "Ali Reza - Full Stack Developer",
     },
-    "facebook": {
-      "title": "Ali Reza - Full Stack Developer",
-      "description": "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-      "url": "https://iamali.vercel.app/",
-      "image": "assets/images/4E6A5969.jpeg"
-    },
-    "linkedin": {
-      "title": "Ali Reza - Full Stack Developer",
-      "description": "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-      "url": "https://iamali.vercel.app/",
-      "image": "assets/images/4E6A5969.jpeg"
-    },
-    "instagram": {
-      "title": "Ali Reza - Full Stack Developer",
-      "description": "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-      "url": "https://iamali.vercel.app/",
-      "image": "assets/images/4E6A5969.jpeg"
-    },
-    "tiktok": {
-      "title": "Ali Reza - Full Stack Developer",
-      "description": "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
-      "url": "https://iamali.vercel.app/",
-      "image": "assets/images/4E6A5969.jpeg"
-    }
-  }
-  
+  },
+  facebook: {
+    title: "Ali Reza - Full Stack Developer",
+    description:
+      "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+    url: "https://www.imali.dev/",
+    image: "assets/images/4E6A5969.jpeg",
+  },
+  linkedin: {
+    title: "Ali Reza - Full Stack Developer",
+    description:
+      "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+    url: "https://www.imali.dev/",
+    image: "assets/images/4E6A5969.jpeg",
+  },
+  instagram: {
+    title: "Ali Reza - Full Stack Developer",
+    description:
+      "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+    url: "https://www.imali.dev/",
+    image: "assets/images/4E6A5969.jpeg",
+  },
+  tiktok: {
+    title: "Ali Reza - Full Stack Developer",
+    description:
+      "Ali Reza is a skilled full-stack web developer with expertise in React.js, Next.js, Laravel, PHP, JavaScript, MySQL, TailwindCSS, Flask, Python, Flutter, and more. He has a passion for transforming innovative ideas into reality, crafting pixel-perfect web solutions that push boundaries and exceed expectations.",
+    url: "https://www.imali.dev/",
+    image: "assets/images/4E6A5969.jpeg",
+  },
+};
