0785138bf106dfb2a130d91be4961f8932171565
Ali reza <aliraza.r10@gmail.com>
2023-06-14 02:11:38 +0500

    source code address added

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -1,7 +1,18 @@
 export default function Footer() {
   return (
-    <footer className="text-center text-primary-100 p-5">
-      <p className="text-center text-primary-100 text-xs font-bold">
+    <footer className="text-center text-primary-100 p-5 space-y-2">
+      <p className="text-center text-primary-100 font-bold text-base leading-6">
+        Find Source code:{" "}
+        <a
+          href="https://github.com/alirazar10/ArFolio"
+          target="_blank"
+          rel="noreferrer"
+          className="text-accent-500 font-bold"
+        >
+          ArFolio
+        </a>{" "}
+      </p>
+      <p className="text-center text-primary-100 text-sm font-semibold">
         Made with ☕ and <span> &#x2764;&#xFE0F; </span> by Ali Reza &copy;{" "}
         {new Date().getFullYear()}
       </p>
