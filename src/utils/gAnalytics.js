f7d14b16b1ffebce1c11f3761bf6278c35e41133
Ali Reza <aliraza.r10@gmail.com>
2025-12-01 05:31:50 +0500

    Remove react-ga and update to gtag for React 19 compatibility

--- src/utils/gAnalytics.js
+++ src/utils/gAnalytics.js
@@ -1,8 +1,17 @@
-import ReactGA from "react-ga";
-
 export const initGA = () => {
   try {
-    ReactGA.initialize("G-WZT6WEBLWV");
+    // Load gtag script if not already loaded
+    if (!window.gtag) {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=G-WZT6WEBLWV';
+      document.head.appendChild(script);
+
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function() { window.dataLayer.push(arguments); };
+      window.gtag('js', new Date());
+    }
+    window.gtag('config', 'G-WZT6WEBLWV');
   } catch (error) {
     console.log(error);
   }
@@ -10,8 +19,11 @@ export const initGA = () => {
 
 export const logPageView = () => {
   try {
-    ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname);
+    if (window.gtag) {
+      window.gtag('config', 'G-WZT6WEBLWV', {
+        page_path: window.location.pathname,
+      });
+    }
   } catch (error) {
     console.log(error);
   }
